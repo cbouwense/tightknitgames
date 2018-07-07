d998d9668f9c1f866592236c1217ee30f0d3e3fa
Christian <bouwensec@gmail.com>
2018-07-07 17:00:40 -0400

    Modified main view to always have the TightKnitGames header. Created game.handlebars view as a generic game page. Created unfinished route for /games and /games/name, and a dummy route for Go Up for development.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const constructorMethod = app => {
 
   app.get('/', (req, res) => {
+    console.log('GET /' );
     res.render('index', { title: 'Tight Knit Games'});
+  });
+
+  // TODO
+  app.get('/games', (req, res) => {
+    console.log('GET /games' );
+    res.sendStatus(404);
+  });
+
+  app.get('/games/go-up', (req, res) => {
+    console.log('GET /games/go-up');
+    console.log('dummy route as an example for dev purposes');
+  });
+
+  // TODO: Hook this up to db
+  app.get('/games/:name', (req, res) => {
+    let gameName = req.params['name'];
+    console.log('GET /games/' + gameName);
+    res.render('game', {title: gameName + ' - Tight Knit Games',
+                        name: gameName})
   });
 
 };
