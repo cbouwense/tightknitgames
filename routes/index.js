7c44016b18c0ed39d49f784b4f37b549d8ee5da6
Christian <bouwensec@gmail.com>
2018-07-07 17:36:41 -0400

    created folder for Go Up images, supplied more info to game.handlebars in routes, got three pics and header for actual view. Other small improvements with views.

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,17 @@ const constructorMethod = app => {
   app.get('/games/go-up', (req, res) => {
     console.log('GET /games/go-up');
     console.log('dummy route as an example for dev purposes');
+    let gameName = 'Go Up';
+    res.render('game', {title: gameName + ' - Tight Knit Games',
+                        name: gameName,
+                        blurb: 'Lay eggs and bounce on them to go up.',
+                        genre: '2D Action Platformer',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                        image1URL: '../public/images/go-up/red.png',
+                        image2URL: '../public/images/go-up/green.png',
+                        image3URL: '../public/images/go-up/blue.png',
+                    });
+
   });
 
   // TODO: Hook this up to db
